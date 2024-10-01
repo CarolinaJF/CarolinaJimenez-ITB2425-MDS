f32f313464470cd1e82bc103ef14463f3c5f1eb3
CarolinaJimenez-ITB2425 <carolina.jimenez.7e8@itb.cat>
2024-10-01 12:55:28 +0200

    # This is a comment
    """comprovarEdat"""

---
+++ TA02/comprovarEdat.py
@@ -0,0 +1,13 @@
+"""
+Carolina Jimenez Fernandez
+01/10/2024
+ASIXc MSD TA02
+Descripció: Exemple dels apunts
+
+"""
+# Programa que demana l'edat i diu si ets major d'edat.
+edat=int(input("Quina edat tens?"))
+if edat>=18:
+    print("Ets major d'edat")
+
+print("Programa Finalitzat")
