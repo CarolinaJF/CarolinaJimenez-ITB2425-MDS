58d9eaeea489fb3bc19edf8fde5b4907b75b18de
CarolinaJimenez-ITB2425 <carolina.jimenez.7e8@itb.cat>
2024-10-08 12:31:58 +0200

    PDF

--- TA03/dataneixament.py
+++ TA03/dataneixament.py
@@ -1,2 +1,33 @@
 # Programa que demana la data de neixament.
-data=int(input("Quan vas neixer?"))
+
+dia=int(input("Dia?"))
+mes=int(input("Mes?"))
+any=int(input("Any?"))
+
+#Validar les dades
+
+from datetime import datetime
+
+current_second = datetime.now().second
+current_minute = datetime.now().minute
+current_hour = datetime.now().hour
+
+current_dat = datetime.now().day
+current_month = datetime.now().hour
+current_year = datetime.now().year
+
+if any>current_year:
+    print("Any incorrecte")
+    print("Programa Finalitzat")
+
+if mes>12 or mes<1:
+    print("Mes incorrecte")
+    print("Programa Finalitzat")
+
+if dia>31 or dia<1:
+    print("Dia incorrecte")
+    print("Programa Finalitzat")
+
+#Procesar les dades
+#todo calcular edat
+print("Programa Finalitzat")
